3bb919f9c07aa852b1afac3fa44e79f85d79ab78
JhonanCaleb <jhonancalebm@gmail.com>
2023-04-08 17:02:49 -0500

    estilos de header a archivo css e importacióne n los demas archivos css

--- Views/inc/header.php
+++ Views/inc/header.php
@@ -1,98 +1,3 @@
-<style>
-  .headerMainBox {
-    width: 100%;
-    height: 85px;
-    background: var(--c_slate);
-    display: flex;
-  }
-
-  .headerMain {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  .headerMain__logo {
-    width: 20em;
-  }
-
-  .headerMain__imgLogo {
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
-  }
-
-  .nav {
-    position: relative;
-    display: grid;
-    place-items: center;
-  }
-
-  .nav i {
-    font-size: xx-large;
-  }
-
-  .nav__bar {
-    position: absolute;
-    z-index: 100;
-    top: 100%;
-    right: 10%;
-    width: 15em;
-    padding: 10px 0 0 0;
-    background-color: var(--c_darkBlue);
-    border-radius: 5px;
-    pointer-events: none;
-    opacity: 0;
-    transition: opacity 0.3s;
-  }
-
-  .nav__bar.show {
-    pointer-events: all;
-    opacity: 1;
-  }
-
-  .nav__username {
-    display: block;
-    text-align: center;
-    text-transform: uppercase;
-    color: white;
-    font-size: small;
-    font-weight: bold;
-    margin: 0 10px;
-  }
-
-  .nav__ul {
-    width: 100%;
-    height: auto;
-    list-style: none;
-    margin-top: 5px;
-  }
-
-  .nav__item {
-    width: 100%;
-    background: inherit;
-  }
-
-  .nav__item:hover {
-    backdrop-filter: brightness(110%);
-  }
-
-  .nav__link {
-    color: white;
-    display: flex;
-    align-items: center;
-    gap: 5px;
-    text-decoration: none;
-    font-size: 14px;
-    padding: 8px 10px;
-    cursor: pointer;
-  }
-
-  .nav__link i {
-    font-size: medium;
-  }
-</style>
 <div class="headerMainBox">
   <header class="headerMain container">
     <a href="./" class="headerMain__logo">
@@ -111,6 +16,24 @@
           </li>
           <li class="nav__item">
             <a class="nav__link" id="btnLogout"><i class="ph-sign-out"></i> Cerrar Sesión</a>
+          </li>
+        </ul>
+      </div>
+      <div class="nav__res__bar">
+        <button class="nav__res__iconClose"><i class="ph ph-x"></i></button>
+        <div class="nav__res__bar__logo">
+          <img class="nav__res__bar__imgLogo" src="<?php echo SERVER_URL; ?>/Views/assets/iconLogo.png" alt="logo" />
+        </div>
+        <span class="nav__res__username"><?php echo $_SESSION['nombres'] . " " . $_SESSION['apellidos'] ?></span>
+        <ul class="nav__res__ul">
+          <li class="nav__res__item">
+            <a class="nav__res__link" href="">Proyectos de mi carrera</a>
+          </li>
+          <li class="nav__res__item">
+            <a class="nav__res__link" href="">Todos los proyectos</a>
+          </li>
+          <li class="nav__res__item">
+            <a class="nav__res__link" id="btnLogout">Cerrar Sesión</a>
           </li>
         </ul>
       </div>
