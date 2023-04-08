4d25a4756d94344839cb8f43e3874f0eb130fb22
JhonanCaleb <jhonancalebm@gmail.com>
2023-04-08 13:03:36 -0500

    vista instructor

--- Views/contents/instructor-view.php
+++ Views/contents/instructor-view.php
@@ -1,1 +1,89 @@
-instructor view
+<section class="section__main">
+  <main class="main container">
+    <div class="top">
+      <h1>Proyectos designados</h1>
+      <a href="">Ver proyectos publicados</a>
+    </div>
+    <hr />
+    <div class="container__projects">
+      <nav class="filter">
+        <ul>
+          <li><a class="filter__item" href="" data-state="">Todos</a></li>
+          <li><a class="filter__item" href="" data-state="">Por revisar</a></li>
+          <li><a class="filter__item" href="" data-state="">Observados</a></li>
+          <li><a class="filter__item" href="" data-state="">Aprobados</a></li>
+          <li><a class="filter__item" href="" data-state="">Desaprobados</a></li>
+          <?php
+          // $sql = mainModel::connect()->prepare("SELECT estado_id,nombre FROM estados");
+          // $sql->execute();
+          // $states = $sql->fetchAll();
+          // foreach ($states as $state) {
+          //   echo '
+          //       <li><a class="filter__item" href="" data-state="' . $state["estado_id"] . '">' . $state["nombre"] . '</a></li>
+          //       ';
+          // }
+          ?>
+        </ul>
+      </nav>
+      <div id="projectsBox" class="projectsBox">
+        <article class="project" style="--cl:black">
+          <a href="<?php echo SERVER_URL; ?>/project" class="project__link"></a>
+          <h1 class="project__title">SISTEMA DE REPOSITORIO</h1>
+          <h2 class="project__type">PROYECTO DE MEJORA</h2>
+          <p class="project__descri">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi maiores quod corrupti dolores eligendi. Sequi, esse delectus? Dolores, mollitia unde. Architecto doloribus error voluptas tenetur, ullam qui placeat modi ipsum!
+          </p>
+          <span class="project__state">ENVIADO</span>
+        </article>
+        <article class="project" style="--cl:black">
+          <a href="<?php echo SERVER_URL; ?>/project" class="project__link"></a>
+          <h1 class="project__title">SISTEMA DE REPOSITORIO</h1>
+          <h2 class="project__type">PROYECTO DE MEJORA</h2>
+          <p class="project__descri">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi maiores quod corrupti dolores eligendi. Sequi, esse delectus? Dolores, mollitia unde. Architecto doloribus error voluptas tenetur, ullam qui placeat modi ipsum!
+          </p>
+          <span class="project__state">ENVIADO</span>
+        </article>
+        <article class="project" style="--cl:black">
+          <a href="<?php echo SERVER_URL; ?>/project" class="project__link"></a>
+          <h1 class="project__title">SISTEMA DE REPOSITORIO</h1>
+          <h2 class="project__type">PROYECTO DE MEJORA</h2>
+          <p class="project__descri">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi maiores quod corrupti dolores eligendi. Sequi, esse delectus? Dolores, mollitia unde. Architecto doloribus error voluptas tenetur, ullam qui placeat modi ipsum!
+          </p>
+          <span class="project__state">ENVIADO</span>
+        </article>
+        <article class="project" style="--cl:black">
+          <a href="<?php echo SERVER_URL; ?>/project" class="project__link"></a>
+          <h1 class="project__title">SISTEMA DE REPOSITORIO</h1>
+          <h2 class="project__type">PROYECTO DE MEJORA</h2>
+          <p class="project__descri">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi maiores quod corrupti dolores eligendi. Sequi, esse delectus? Dolores, mollitia unde. Architecto doloribus error voluptas tenetur, ullam qui placeat modi ipsum!
+          </p>
+          <span class="project__state">ENVIADO</span>
+        </article>
+        <article class="project" style="--cl:black">
+          <a href="<?php echo SERVER_URL; ?>/project" class="project__link"></a>
+          <h1 class="project__title">SISTEMA DE REPOSITORIO</h1>
+          <h2 class="project__type">PROYECTO DE MEJORA</h2>
+          <p class="project__descri">Lt o erendis facere fugit deleniti nobis excepturi doloribus rem et, incidunt ipsa dicta accusamus quaerat, nisi exercitationem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi maiores quod corrupti dolores eligendi. Sequi, esse delectus? Dolores, mollitia unde. Architecto doloribus error voluptas tenetur, ullam qui placeat modi ipsum!
+          </p>
+          <span class="project__state">ENVIADO</span>
+        </article>
+        <article class="project" style="--cl:black">
+          <a href="<?php echo SERVER_URL; ?>/project" class="project__link"></a>
+          <h1 class="project__title">SISTEMA DE REPOSITORIO</h1>
+          <h2 class="project__type">PROYECTO DE MEJORA</h2>
+          <p class="project__descri">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi maiores quod corrupti dolores eligendi. Sequi, esse delectus? Dolores, mollitia unde. Architecto doloribus error voluptas tenetur, ullam qui placeat modi ipsum!
+          </p>
+          <span class="project__state">ENVIADO</span>
+        </article>
+        <article class="project" style="--cl:black">
+          <a href="<?php echo SERVER_URL; ?>/project" class="project__link"></a>
+          <h1 class="project__title">SISTEMA DE REPOSITORIO</h1>
+          <h2 class="project__type">PROYECTO DE MEJORA</h2>
+          <p class="project__descri">ingaiores quod corrupti dolores eligendi. Sequi, esse delectus? Dolores, mollitia unde. Architecto doloribus error voluptas tenetur, ullam qui placeat modi ipsum!
+          </p>
+          <span class="project__state">ENVIADO</span>
+        </article>
+      </div>
+    </div>
+  </main>
+</section>
+<!-- <script src="<?php echo SERVER_URL; ?>/Views/js/alerts.js"></script> -->
--- Views/js/main.js
+++ Views/js/main.js
@@ -5,5 +5,13 @@ iconUser.addEventListener("click", (e) => {
   userBar.classList.toggle("show");
 });
 
+// Limitar palabras de la descripción de los trámites/proyectos
+const allDescrips = document.querySelectorAll(".project__descri");
 
+allDescrips.forEach((descri) => {
+  let words = descri.textContent.split(" ");
 
+  if (words.length > 20) {
+    descri.textContent = words.slice(0, 20).join(" ") + "...";
+  }
+});
