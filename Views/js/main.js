4fcb0398cdf8d4be8babf9bec11fad3c3badacbc
JhonanCaleb <jhonancalebm@gmail.com>
2023-04-08 17:03:46 -0500

    agregación de la funcion toggleShow

--- Views/js/main.js
+++ Views/js/main.js
@@ -1,8 +1,19 @@
 // togle de barra de usuario opciones
-const userBar = document.getElementById("userBar");
+const userBar = document.querySelector(".nav__bar");
+const userBarRes = document.querySelector(".nav__res__bar");
 const iconUser = document.getElementById("iconUser");
+const btnCloseUserBar = document.querySelector(".nav__res__iconClose");
+
+function toggleShowElement(element) {
+  element.classList.toggle("show");
+}
 iconUser.addEventListener("click", (e) => {
-  userBar.classList.toggle("show");
+  toggleShowElement(userBar);
+  toggleShowElement(userBarRes);
+});
+btnCloseUserBar.addEventListener("click", (e) => {
+  toggleShowElement(userBar);
+  toggleShowElement(userBarRes);
 });
 
 // Limitar palabras de la descripción de los trámites/proyectos
