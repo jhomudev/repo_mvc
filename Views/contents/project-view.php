e43e2e4229a452b447a122f270e4c9a004933061
JhonanCaleb <jhonancalebm@gmail.com>
2023-04-27 16:29:23 -0500

    mostrar observaciones

--- Views/contents/project-view.php
+++ Views/contents/project-view.php
@@ -1,7 +1,7 @@
 <?php
-require_once "Controllers/projectController.php";
+require_once "Controllers/ProjectController.php";
 
-$PI = new projectController();
+$PI = new ProjectController();
 
 $data = $PI->getInfoProjectController();
 $project = $data['project'];
@@ -11,16 +11,14 @@ $obs = $data['observations'];
 ?>
 <header class="header">
   <a href="<?php echo SERVER_URL; ?>/login" class="header__btnBack"><i class="ph ph-arrow-left"></i></a>
-  <h3 class="header__proyect">PROYECTO DE
-    <?php echo ($project['tipo'] == 1) ? 'INNOVACIÓN' : (($project['tipo'] == 2) ? 'MEJORA' : (($project['tipo'] == 3) ? 'CREATIVIDAD' : ''));
-    ?></h3>
+  <h3 class="header__proyect">PROYECTO DE <?php echo $project['tipo']; ?></h3>
   <strong class="header__titleproyect"><?php echo $project['titulo'] ?></strong>
 </header>
 <main class="main">
   <div class="detailsResult">
-    <div class="detailsResult__flex" style="--cl:<?php echo $project['clr']; ?>;">
+    <div class="detailsResult__flex" style="--cl:<?php echo $project['color']; ?>;">
       <strong class="detailsResult__state"><i class="ph ph-file"></i> <?php echo $project['estado'] ?></strong>
-      <p>Enviado <?php echo $project['fecha_gen'] ?></p>
+      <p>Enviado <?php echo date('d-m-Y H:i', strtotime($project['fecha_gen'])) ?></p>
     </div>
     <i class="detailsResult__qualification"><?php echo isset($project['nota']) ? $project['nota'] : 'Sin calificar' ?></i>
   </div>
@@ -32,6 +30,15 @@ $obs = $data['observations'];
           <i class="ph ph-caret-down"></i>
         </summary>
         <div class="proyect__content">
+          <?php
+          if ($_SESSION['tipo'] == USER_TYPE['student']) {
+          ?>
+            <div class="proyect__stateInfo" style="--cl:<?php echo $project['color']; ?>;">
+              <p><?php echo $project['es_descrip'] ?></p>
+            </div>
+          <?php
+          }
+          ?>
           <h4 class="proyect__part">Descripción del proyecto</h4>
           <p class="proyect__description">
             <?php echo $project['descripcion'] ?>
@@ -64,24 +71,37 @@ $obs = $data['observations'];
           <?php
           if (count($obs) > 0) {
             foreach ($obs as $key => $ob) {
-              echo '
-                <article class="observation">
-                  <div class="observation__flex">
-                    <h3 class="observation__author">' . $ob['nombres'] . ' ' . $ob['apellidos'] . '</h3>
-                    <span class="observation__date">' . $ob['fecha_gen'] . '</span>
+          ?>
+              <article class="observation">
+                <div class="observation__flex">
+                  <div class="observation__data">
+                    <h3 class="observation__author"><?php echo $ob['nombres'] . ' ' . $ob['apellidos']; ?></h3>
+                    <span class="observation__date"><?php echo date('d-m-Y H:i', strtotime($ob['fecha_gen'])); ?></span>
                   </div>
-                  <p class="observation__descri">' . $ob['descripcion'] . '</p>
-                </article>
-                ';
+                  <?php
+                  if ($_SESSION['usuario_id'] == $ob['autor_id']) {
+                  ?>
+                    <form action="<?php echo SERVER_URL; ?>/fetch/observationFetch.php" method="POST" class="observation__form formFetch">
+                      <input type="hidden" name="observacion_id" value="<?php echo $ob['observacion_id']; ?>">
+                      <button type="submit" title="Eliminar observación" class="observation__form__submit"><i class="ph ph-trash"></i></button>
+                    </form>
+                  <?php
+                  }
+                  ?>
+                </div>
+                <p class="observation__descri"><?php echo $ob['descripcion']; ?></p>
+              </article>
+          <?php
             }
           } else echo '<i>EL proyecto no tiene observaciones</i>';
 
-          if ($_SESSION['tipo'] !== 3) {
+          if ($_SESSION['tipo'] !== 3 && $project['estado_id'] < 4) {
             echo '
               <div class="newObs">
                 <button class="newObs__btnNewObs">Nueva observación</button>
-                <form action="" class="newObs__form">
-                  <textarea name="tx_newObs" class="newObs__form__textarea" id="obs" placeholder="Detalle su observación aquí"></textarea>
+                <form action="' . SERVER_URL . '/fetch/observationFetch.php" method="POST" class="newObs__form formFetch">
+                  <input type="hidden" name="project_id" value="' . $project['proyecto_id'] . '">
+                  <textarea name="descryption" class="newObs__form__textarea" id="obs" placeholder="Detalle su observación aquí"></textarea>
                   <input type="submit" value="Enviar" class="newObs__form__submit">
                 </form>
               </div>
