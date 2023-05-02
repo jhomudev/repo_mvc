31636afebcb19bd29d156868bdc67251e8e37498
JhonanCaleb <jhonancalebm@gmail.com>
2023-05-02 02:52:31 -0500

    arreglos

--- Models/projectModel.php
+++ Models/projectModel.php
@@ -13,7 +13,7 @@ class ProjectModel extends ObservationModel
     $user_sede = $params['user_sede'];
     $filter_state = $params['filter_state'];
     $words = $params['words'];
-    if (isset($words)) $query = "SELECT p.proyecto_id,p.titulo,p.tipo,p.descripcion,e.nombre AS estado,e.descripcion AS es_descrip,e.color, MAX(dt.fecha_publicacion) AS fecha_publicacion, GROUP_CONCAT(a.nombres,' ',a.apellidos SEPARATOR ', ')  AS autores FROM proyectos p INNER JOIN tramites t ON t.proyecto_id=p.proyecto_id INNER JOIN usuarios a ON a.usuario_id=t.estudiante_id INNER JOIN detalle_tramite dt ON dt.tramite_id=t.tramite_id INNER JOIN estados e ON e.estado_id=dt.estado_id /* WHERE dt.estado_id=7 */ WHERE p.titulo LIKE '%" . $words . "%' GROUP BY p.proyecto_id,p.titulo,p.tipo,p.descripcion,e.nombre,e.descripcion,e.color";
+    if (isset($words)) $query = "SELECT p.proyecto_id,p.titulo,p.tipo,p.descripcion,e.nombre AS estado,e.descripcion AS es_descrip,e.color, MAX(dt.fecha_publicacion) AS fecha_publicacion, GROUP_CONCAT(a.nombres,' ',a.apellidos SEPARATOR ', ')  AS autores FROM proyectos p INNER JOIN tramites t ON t.proyecto_id=p.proyecto_id INNER JOIN usuarios a ON a.usuario_id=t.estudiante_id INNER JOIN detalle_tramite dt ON dt.tramite_id=t.tramite_id INNER JOIN estados e ON e.estado_id=dt.estado_id /* WHERE dt.estado_id=7 */ WHERE p.titulo LIKE '%$words%' GROUP BY p.proyecto_id,p.titulo,p.tipo,p.descripcion,e.nombre,e.descripcion,e.color";
     if (empty($filter_state)) {
       if ($user_type == USER_TYPE['student']) $query = "SELECT p.proyecto_id,p.titulo,p.tipo,p.descripcion,e.nombre AS estado,e.descripcion AS es_descrip,e.color FROM proyectos p INNER JOIN tramites t ON t.proyecto_id=p.proyecto_id INNER JOIN detalle_tramite dt ON dt.tramite_id=t.tramite_id INNER JOIN estados e ON e.estado_id=dt.estado_id  WHERE t.estudiante_id=$user_id GROUP BY p.proyecto_id,p.titulo,p.tipo,p.descripcion,e.nombre,e.descripcion,e.color ORDER BY dt.estado_id";
 
@@ -39,7 +39,7 @@ class ProjectModel extends ObservationModel
   }
 
   // Funcion para obtener datos de un tramite u proyecto
-  protected static function getInfoProjectModel($project_id): array
+  public static function getInfoProjectModel($project_id): array
   {
     // obtencion de datos de proyecto
     $query_project = "SELECT p.proyecto_id, p.nombre_archivo, p.titulo, p.tipo, p.descripcion,t.estudiante_generador,t.fecha_gen,dt.instructor_id, (SELECT CONCAT(nombres,' ',apellidos) FROM usuarios WHERE usuario_id=dt.instructor_id) AS instructor , dt.estado_id, dt.jurados,dt.fecha_revision,dt.fecha_sustentacion,dt.fecha_publicacion, dt.nota, e.nombre AS estado, e.color,e.descripcion AS es_descrip, esc.nombre AS escuela, c.nombre AS carrera,c.carrera_id
@@ -54,8 +54,15 @@ class ProjectModel extends ObservationModel
     GROUP BY p.proyecto_id, p.titulo, p.tipo, p.descripcion,t.estudiante_generador, t.fecha_gen, dt.instructor_id, dt.estado_id, dt.jurados,dt.fecha_revision,dt.fecha_sustentacion,dt.fecha_publicacion, dt.nota, e.nombre,e.color,e.descripcion, esc.nombre, c.nombre,c.carrera_id, u.nombres, u.apellidos";
     $project = MainModel::executeQuerySimple($query_project)->fetch();
 
-
     // obtención de los datos de los autores
+    $array_ids_juries = explode(",", $project['jurados']);
+    $juries_names = [];
+    foreach ($array_ids_juries as $key => $id_jury) {
+      $jury = MainModel::executeQuerySimple("SELECT CONCAT(nombres,' ', apellidos) AS fullname FROM usuarios WHERE usuario_id=$id_jury");
+      array_push($juries_names, $jury->fetchColumn());
+    }
+
+    // obtención de los datos de los jurados
     $queryAuthors = "SELECT u.usuario_id,u.nombres,u.apellidos,u.sede_id,u.carrera_id,dt.detalle_id,dt.nota
     FROM usuarios u 
     INNER JOIN tramites t ON u.usuario_id=t.estudiante_id
@@ -70,6 +77,7 @@ class ProjectModel extends ObservationModel
       "authors" => $authors,
       "project" => $project,
       "observations" => $obs,
+      "juries" => $juries_names,
     ];
 
     return $data;
