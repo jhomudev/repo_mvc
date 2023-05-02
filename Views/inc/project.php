a3485116d9420075579765c317cb257dce37f1af
JhonanCaleb <jhonancalebm@gmail.com>
2023-05-01 22:14:05 -0500

    vista de proyecto en repo

--- Views/inc/project.php
+++ Views/inc/project.php
@@ -1,35 +1,54 @@
+<?php
+require_once "Controllers/ProjectController.php";
+$IP = new ProjectController();
+$data = $IP->getInfoProjectController();
+
+$project = $data['project'];
+$authors = $data['authors'];
+echo '
+  <script>
+    console.log(' . json_encode($project) . ')
+  </script>
+';
+?>
 <section class="flex container" container>
   <main class="projectBox">
-    <h1 class="project__title">Ingeniería inversa para la adaptación tecnológica en una empresa manufacturera peruana, estudio de caso</h1>
+    <h1 class="project__title"><?php echo $project['titulo'] ?></h1>
     <h3 class="projetc__desc__text enc">Descripción del proyecto</h3>
-    <p class="project__descri">El sector manufacturero se ha convertido en uno de los ejes de la economía peruana por sus altas tasas de crecimiento a partir de la liberación comercial de la década de los noventa generando nuevos empleos, ingresos fiscales y desarrollo de pequeñas empresas que producen partes y servicios para este sector. Evidentemente, su crecimiento y competitividad está fuertemente relacionado con la innovación tecnológica donde un 65,5% de las empresas manufactureras desarrolló al menos una actividad de innovación en el periodo 2009-2011 (CONCYTEC, 2013). Cabe destacar que estas actividades de innovación tienen como objetivo final mejorar los resultados de la empresa al desarrollar y poner en marcha nuevos productos y procesos ("Oslo manual," 2005). Estas van desde actividades en investigación y desarrollo interno y externo, transferencia tecnológica, adquisiciones de bienes de capital y TIC’s, capacitación, hasta las de diseño e ingeniera industrial. A propósito, el resultado de la primera Encuesta Nacional de Innovación en la Industria Manufacturera muestra que el 78,4% de las empresas centran sus esfuerzos en la adquisición de bienes de capital (INEI, 2012). Esta falta de equilibrio entre las actividades de innovación, pone en riesgo el desarrollo de capacidades endógenas y es un factor que explica el subdesarrollo tal como señala Máximo Vega (2003, p. 32): “una indagación global e histórica sobre las causas del subdesarrollo llevo a evaluar la relación de las economías subdesarrolladas con las desarrolladas (…) se trata esta vez de la dependencia como forma de relación con resultados predeterminados y distribución no equitativa de los frutos”.</p>
+    <p class="project__descri"><?php echo $project['descripcion'] ?></p>
     <hr>
     <div class="project__tableBox">
       <table class="project__table">
         <tbody>
           <tr>
             <th class="table__th">Autores: </th>
-            <td>Cabrera Donayre, Rony Martin</td>
+            <td>
+              <?php
+              foreach ($authors as $key => $author) {
+                echo $author['nombres'] . ' ' . $author['apellidos'];
+              }
+              ?>
+            </td>
           </tr>
           <tr>
             <th class="table__th">Formato: </th>
-            <td>Proyecto de mejora</td>
+            <td>Proyecto de <?php echo $project['tipo']?></td>
           </tr>
           <tr>
             <th class="table__th">Fecha de publicación: </th>
-            <td>2015</td>
+            <td><?php echo date("d-m-Y",strtotime($project['fecha_publicacion']))?></td>
           </tr>
           <tr>
             <th class="table__th">Escuela: </th>
-            <td>Esuela de tecnoloías de la información</td>
+            <td><?php echo $project['escuela']?></td>
           </tr>
           <tr>
             <th class="table__th">Carrera: </th>
-            <td>Ingeniería de Softweare con IA</td>
+            <td><?php echo $project['carrera']?></td>
           </tr>
           <tr>
             <th class="table__th">Enlace de recurso: </th>
-            <td><a class="table__link" target="_blank" href="https://ucrindex.ucr.ac.cr/wp-content/uploads/docs/Software_Repositorios.pdf">Click aquí</a></td>
+            <td><a class="table__link" target="_blank" href="<?php echo SERVER_URL.'/uploads/'.$project['nombre_archivo']?>">Click aquí</a></td>
           </tr>
         </tbody>
       </table>
@@ -37,33 +56,31 @@
     </div>
   </main>
   <aside class="others">
-    <h3 class="others__text enc">Proyectos similares</h3>
+    <h3 class="others__text enc">Otros proyectos</h3>
     <div class="othersBox">
-      <article class="others__article">
-        <a href="" class="other__title">Ingeniería inversa para la adaptación tecnológica en una empresa manufacturera peruana, estudio de caso</a>
-        <p class="other__authors">por: Cabrera Donayre, Rony Martin</p>
-        <b class="others__date">Publicado el 2015</b>
-      </article>
-      <article class="others__article">
-        <a href="" class="other__title">Ingeniería inversa para la adaptación tecnológica en una empresa manufacturera peruana, estudio de caso</a>
-        <p class="other__authors">por: Cabrera Donayre, Rony Martin</p>
-        <b class="others__date">Publicado el 2015</b>
-      </article>
-      <article class="others__article">
-        <a href="" class="other__title">Ingeniería inversa para la adaptación tecnológica en una empresa manufacturera peruana, estudio de caso</a>
-        <p class="other__authors">por: Cabrera Donayre, Rony Martin</p>
-        <b class="others__date">Publicado el 2015</b>
-      </article>
-      <article class="others__article">
-        <a href="" class="other__title">Ingeniería inversa para la adaptación tecnológica en una empresa manufacturera peruana, estudio de caso</a>
-        <p class="other__authors">por: Cabrera Donayre, Rony Martin</p>
-        <b class="others__date">Publicado el 2015</b>
-      </article>
-      <article class="others__article">
-        <a href="" class="other__title">Ingeniería inversa para la adaptación tecnológica en una empresa manufacturera peruana, estudio de caso</a>
-        <p class="other__authors">por: Cabrera Donayre, Rony Martin</p>
-        <b class="others__date">Publicado el 2015</b>
-      </article>
+      <?php 
+        $carrera_id=$project['carrera_id'];
+        $proyecto_id=$project['proyecto_id'];
+        $query = "SELECT p.proyecto_id,p.titulo,p.tipo,p.descripcion,e.nombre AS estado,e.descripcion AS es_descrip,e.color, MAX(dt.fecha_publicacion) AS fecha_publicacion, GROUP_CONCAT(a.nombres,' ',a.apellidos SEPARATOR ', ')  AS autores FROM proyectos p 
+        INNER JOIN tramites t ON t.proyecto_id=p.proyecto_id 
+        INNER JOIN usuarios a ON a.usuario_id=t.estudiante_id 
+        INNER JOIN detalle_tramite dt ON dt.tramite_id=t.tramite_id 
+        INNER JOIN estados e ON e.estado_id=dt.estado_id 
+        WHERE a.carrera_id=$carrera_id AND p.proyecto_id<>'$proyecto_id'
+        GROUP BY p.proyecto_id,p.titulo,p.tipo,p.descripcion,e.nombre,e.descripcion,e.color";
+        $others_projects=MainModel::executeQuerySimple($query );
+        $others_projects=$others_projects->fetchAll();
+
+        foreach ($others_projects as $key => $project) {
+          echo '
+          <article class="others__article">
+            <a href="'.SERVER_URL.'/repository?id='.$project['proyecto_id'].'" class="other__title">'.$project['titulo'].'</a>
+            <p class="other__authors">por: '.$project['autores'].'</p>
+            <b class="others__date">Publicado el '.date("Y",strtotime($project['fecha_publicacion'])).'</b>
+          </article>    
+          ';
+        }
+      ?>
     </div>
   </aside>
 </section>
