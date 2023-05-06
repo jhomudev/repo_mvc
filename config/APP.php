040020dafe9eec5cc0b6a3340d02e002f588bb12
JhonanCaleb <jhonancalebm@gmail.com>
2023-05-06 13:56:29 -0500

    correcciones

--- config/APP.php
+++ config/APP.php
@@ -5,7 +5,7 @@ const COMPANY = "Upload Sen";
 
 const NAMESESSION = "Upload_Sen_session";
 
-const HOST_EMAIL = "Upload_Sen_session";
+const HOST_EMAIL = "UploadSen";
 
 date_default_timezone_set("America/Lima");
 
